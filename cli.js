6b97060ac13fd9393f588798010324f725f774fd
Vince Kruger <vincekruger@gmail.com>
2020-03-29 22:43:54 +0200

    Updated cli

--- cli.js
+++ cli.js
@@ -1,15 +1,42 @@
 const fs = require('fs');
 const path = require('path');
-const {ArgumentParser} = require('argparse');
+const { ArgumentParser } = require('argparse');
+const { createBrowser, convertFile } = require('./index.js');
 
-const {createBrowser, convertFile} = require('./index.js');
+const convertImage = async (file) => {
+  const pathinfo = require('pathinfo')
+  const fs = require('fs')
+  const extractFrames = require('gif-extract-frames')
+  const pi = pathinfo(file)
+  const outputdir = `${pi.dirname}/${pi.basename}-frames`
+
+  /// Check if directory exists
+  if (fs.existsSync(outputdir)) {
+    fs.rmdirSync(outputdir, { recursive: true })
+  }
+
+  /// Create a new directory
+  fs.mkdirSync(outputdir)
+
+  /// Extract Frames
+  const results = await extractFrames({
+    input: file,
+    output: `${outputdir}/frame-%d.png`,
+    coalesce: false
+  })
+
+  /// Output
+  console.log(`Extracted ${pi.basename}`, 'Number of frames', results.shape[0])
+  return true
+}
 
 const convertFiles = async function (filePaths, browser, options) {
   for (const filePath of filePaths) {
     console.log(`Converting ${filePath}...`);
 
     try {
-      await convertFile(filePath, {output: `${filePath}.gif`, browser, ...options});
+      await convertFile(filePath, { output: `${filePath}.gif`, browser, ...options });
+      await convertImage(filePath + '.gif')
     } catch (e) {
       console.error(e);
     }
@@ -20,9 +47,9 @@ const main = async function () {
   const parser = new ArgumentParser({
     description: 'Animated stickers for Telegram (*.tgs) to animated GIFs converter',
   });
-  parser.addArgument('--height', {help: 'Output image height. Default: auto', type: Number});
-  parser.addArgument('--width', {help: 'Output image width. Default: auto', type: Number});
-  parser.addArgument('paths', {help: 'Paths to .tgs files to convert', nargs: '+'});
+  parser.addArgument('--height', { help: 'Output image height. Default: auto', type: Number });
+  parser.addArgument('--width', { help: 'Output image width. Default: auto', type: Number });
+  parser.addArgument('paths', { help: 'Paths to .tgs files to convert', nargs: '+' });
 
   const args = parser.parseArgs();
 
@@ -43,7 +70,7 @@ const main = async function () {
     chromiumPath: process.env['CHROMIUM_PATH'],
     useSandbox: JSON.parse(process.env['USE_SANDBOX'] || 'true'),
   });
-  await convertFiles(paths, browser, {width: args.width, height: args.height});
+  await convertFiles(paths, browser, { width: args.width, height: args.height });
   await browser.close();
 };
 
